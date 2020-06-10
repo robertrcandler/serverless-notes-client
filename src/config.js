20d9dd4daeaee0453d6b55f4a07e9254a7ed6ddc
robertrcandler <robert.candler.32@gmail.com>
2020-06-10 14:51:01 -0500

    connecting app to AWS

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "notes-app-upload3"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://i2mizfqhv9.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_o1uinKD6R",
+      APP_CLIENT_ID: "7q050s3lh7j7l6hnjb5sidredd",
+      IDENTITY_POOL_ID: "us-east-2:52301180-d45a-41ed-8df2-def3a3f45491"
+    }
+  };
