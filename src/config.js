ee87619d0714c30c6959a00c6204ffd973c313d9
robertrcandler <robert.candler.32@gmail.com>
2020-06-14 15:37:04 -0500

    added new note

--- src/config.js
+++ src/config.js
@@ -1,16 +1,17 @@
 export default {
-    s3: {
-      REGION: "us-east-2",
-      BUCKET: "notes-app-upload3"
-    },
-    apiGateway: {
-      REGION: "us-east-2",
-      URL: "https://i2mizfqhv9.execute-api.us-east-2.amazonaws.com/prod"
-    },
-    cognito: {
-      REGION: "us-east-2",
-      USER_POOL_ID: "us-east-2_o1uinKD6R",
-      APP_CLIENT_ID: "7q050s3lh7j7l6hnjb5sidredd",
-      IDENTITY_POOL_ID: "us-east-2:52301180-d45a-41ed-8df2-def3a3f45491"
-    }
-  };
+  MAX_ATTACHMENT_SIZE: 5000000,
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "notes-app-upload3"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://i2mizfqhv9.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_o1uinKD6R",
+    APP_CLIENT_ID: "7q050s3lh7j7l6hnjb5sidredd",
+    IDENTITY_POOL_ID: "us-east-2:52301180-d45a-41ed-8df2-def3a3f45491"
+  }
+};
